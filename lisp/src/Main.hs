ae0bae850d2995b0ee0d571f3fb30785e1cae6bd
Toby Smith <toby@tismith.id.au>
2014-07-08 08:44:58 +1000

    Remove some unneeded functions

--- lisp/src/Main.hs
+++ lisp/src/Main.hs
@@ -27,24 +27,11 @@ readExpr input = case parse parseExpr "lisp" input of
     Left err -> throwError $ Parser err
     Right val -> return val
 
-flushStr :: String -> IO ()
-flushStr str = putStr str >> hFlush stdout
-
-readPrompt :: String -> IO String
-readPrompt prompt = flushStr prompt >> getLine
-
 evalString :: String -> IO String
 evalString expr = return $ extractValue $ trapError (liftM show $ readExpr expr >>= eval)
 
 evalAndPrint :: String -> IO ()
 evalAndPrint expr =  evalString expr >>= putStrLn
-
-until' :: Monad m => (a -> Bool) -> m a -> (a -> m ()) -> m ()
-until' pred prompt action = do
-  result <- prompt
-  if pred result
-     then return ()
-     else action result >> until' pred prompt action
 
 runRepl :: IO ()
 runRepl = do
