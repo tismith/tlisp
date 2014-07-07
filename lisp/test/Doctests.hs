488d70d5e11bc1b54223a4ed4e801ea80254fffd
Toby Smith <toby@tismith.id.au>
2014-07-08 08:38:33 +1000

    Fix up tests after adding the cabal flag

--- lisp/test/Doctests.hs
+++ lisp/test/Doctests.hs
@@ -1,3 +1,9 @@
+{-# LANGUAGE CPP #-}
+
 import Test.DocTest
-main = doctest ["-isrc", "src/Main.hs"]
+main = doctest ["-isrc",
+#ifdef LIBEDITLINE
+        "-DLIBEDITLINE",
+#endif
+        "src/Main.hs"]
 
