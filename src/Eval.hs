2c979658ef8c0c346655d3eb1c5202ddac431a60
Toby Smith <toby@tismith.id.au>
2014-07-09 10:14:43 +1000

    Add case and cond builtins

--- src/Eval.hs
+++ src/Eval.hs
@@ -25,9 +25,66 @@ eval (List [Atom "if", pred, conseq, alt]) =
        case result of
          Bool False -> eval alt
          otherwise -> eval conseq
+eval (List [Atom "if", pred, conseq]) =
+    do result <- eval pred
+       case result of
+         Bool False -> throwError $ Unspecified "if without an else"
+         otherwise -> eval conseq
+eval (List ((Atom "cond"):(c:cs))) =
+    do result <- foldl (chainEvalClause (evalCondClause)) (evalCondClause c) cs
+       case result of
+            Just l -> return l
+            Nothing -> throwError $ Unspecified "no matching cond"
+eval (List ((Atom "case"):key:c:cs)) =
+    do evalKey <- eval key
+       result <- foldl (chainEvalClause (evalCaseClause evalKey)) (evalCaseClause evalKey c) cs
+       case result of
+            Just l -> return l
+            Nothing -> throwError $ Unspecified "no matching case"
 eval (List (Atom func : args)) = mapM eval args >>= apply func
---eval (List (val:[])) = eval val
 eval badForm = throwError $ BadSpecialForm "Unrecognized special form" badForm
+
+chainEvalClause :: (LispVal -> ThrowsError (Maybe LispVal)) -> ThrowsError (Maybe LispVal) -> LispVal -> ThrowsError (Maybe LispVal)
+chainEvalClause evalFunc evalC unevalC = do
+                        clause <- evalC
+                        case clause of
+                            Just l -> return clause
+                            Nothing -> evalFunc unevalC
+
+evalCaseClause :: LispVal -> LispVal -> ThrowsError (Maybe LispVal)
+evalCaseClause key c@(List (datums:(exprs@(_:_)))) = --exprs can't be []
+    case datums of
+        Atom "else" -> do
+                evalExprs <- mapM eval exprs
+                return $ Just $ last evalExprs
+        List l -> do success <- liftM or $ mapM (\x -> (eqv [x, key]) >>= unpackBool) l
+                     if success
+                        then do
+                            evalExprs <- mapM eval exprs
+                            return $ Just $ last evalExprs
+                        else return Nothing
+evalCaseClause _ badForm = throwError $ BadSpecialForm "Unrecognized case clause form" badForm
+
+evalCondClause :: LispVal -> ThrowsError (Maybe LispVal)
+evalCondClause (List (test:[])) =
+    do success <- eval test
+       case success of
+            Bool True -> return $ Just success
+            Bool False -> return Nothing
+            _ -> throwError $ TypeMismatch "boolean" success
+evalCondClause (List (test:exprs)) =
+    case test of
+        Atom "else" -> do
+            evalExprs <- mapM eval exprs
+            return $ Just $ last evalExprs
+        _ -> do success <- eval test
+                case success of
+                    Bool True -> do
+                        evalExprs <- mapM eval exprs
+                        return $ Just $ last evalExprs
+                    Bool False -> return Nothing
+                    _ -> throwError $ TypeMismatch "boolean" success
+evalCondClause badForm = throwError $ BadSpecialForm "Unrecognized cond clause form" badForm
 
 apply :: String -> [LispVal] -> ThrowsError LispVal
 apply func args = maybe (throwError $ NotFunction "Unrecognized primitive function args" func)
@@ -373,6 +430,7 @@ data LispError = NumArgs Integer [LispVal]
                | BadSpecialForm String LispVal
                | NotFunction String String
                | UnboundVar String String
+               | Unspecified String
                | Default String
 
 showError :: LispError -> String
@@ -384,6 +442,7 @@ showError (NumArgs expected found) = "Expected " ++ show expected
 showError (TypeMismatch expected found) = "Invalid type: expected " ++ expected
                                        ++ ", found " ++ show found
 showError (Parser parseErr) = "Parse error at " ++ show parseErr
+showError (Unspecified message) = "Unspecified operation: " ++ message
 
 instance Show LispError where show = showError
 
