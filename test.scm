63175282710733f5b1d48ff1eb68e95df27056bd
Toby Smith <toby@tismith.id.au>
2014-07-28 10:18:43 +1000

    More test cases
    I've got broken set! functionality though, I should be able
    to set! from inside a function, but I end up discarding changes to
    globals made from inside functions

--- test.scm
+++ test.scm
@@ -1,5 +1,12 @@
 (load "stdlib.scm")
 
-(and
+(write "test cases")
+
+(define contfunc #f)
+(write (and
  (cond ((> 1 3) #f) ((> 3 1) #t))
- (case (* 3 2) ((1 2) #f) ((3 4 5 6) #t)))
+ (case (* 3 2) ((1 2) #f) ((3 4 5 6) #t))
+ (case (+ 1 (call-with-current-continuation
+  (lambda (cont)
+    (set! contfunc cont)
+     1))) ((2) #t) (else #f))))
