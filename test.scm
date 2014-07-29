1df240a6d6e605f10de23039f8ce5ff9b1ebed55
Toby Smith <toby@tismith.id.au>
2014-07-29 14:00:10 +1000

    Fix up formatting

--- test.scm
+++ test.scm
@@ -7,19 +7,21 @@
 (define (search wanted? lst)
   (call/cc
     (lambda (return)
-      (for-each (lambda (element)
-		  (if (wanted? element)
-		    (return element)))
-		lst)
+      (for-each
+	(lambda (element)
+	  (if (wanted? element)
+	    (return element)))
+	lst)
       #f)))
 
 (define contfunc #f)
-(write (and
- (cond ((> 1 3) #f) ((> 3 1) #t))
- (case (* 3 2) ((1 2) #f) ((3 4 5 6) #t))
- (= (search (lambda (x) (> x 3)) '(1 2 3 4 5)) 4)
- (case (+ 1 (call-with-current-continuation
-  (lambda (cont)
-    (set! contfunc cont)
-     1))) ((2) #t) (else #f))))
+(write
+  (and
+    (cond ((> 1 3) #f) ((> 3 1) #t))
+    (case (* 3 2) ((1 2) #f) ((3 4 5 6) #t))
+    (= (search (lambda (x) (> x 3)) '(1 2 3 4 5)) 4)
+    (case (+ 1 (call-with-current-continuation
+		 (lambda (cont)
+		   (set! contfunc cont)
+		   1))) ((2) #t) (else #f))))
 
