c73b541867c01fa79439c6c85870e01fa9142561
Toby Smith <toby@tismith.id.au>
2014-07-30 17:34:29 +1000

    Minor change to the test.scm

--- test.scm
+++ test.scm
@@ -1,7 +1,5 @@
 (load "stdlib.scm")
 
-(write "test cases")
-
 (define call/cc call-with-current-continuation)
 
 (define (search wanted? lst)
@@ -19,17 +17,17 @@
       3)
 
 (define contfunc #f)
-(write
-  (and
-    (cond ((> 1 3) #f) ((> 3 1) #t))
-    (case (* 3 2) ((1 2) #f) ((3 4 5 6) #t))
-    (= (search (lambda (x) (> x 3)) '(1 2 3 4 5)) 4)
-    (case (+ 1 (call-with-current-continuation
-		 (lambda (cont)
-		   (set! contfunc cont)
-		   1))) ((2) #t) (else #f))
-    (= 3 (f (lambda (x) x)))
-    (= (call-with-current-continuation f) 2)
-    ))
+
+(and
+  (cond ((> 1 3) #f) ((> 3 1) #t))
+  (case (* 3 2) ((1 2) #f) ((3 4 5 6) #t))
+  (= (search (lambda (x) (> x 3)) '(1 2 3 4 5)) 4)
+  (case (+ 1 (call-with-current-continuation
+	       (lambda (cont)
+		 (set! contfunc cont)
+		 1))) ((2) #t) (else #f))
+  (= 3 (f (lambda (x) x)))
+  (= (call-with-current-continuation f) 2)
+)
 
 
